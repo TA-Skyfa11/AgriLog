7a00b6ddeb67660a94556f838947852fd68218b3
Nhatnk <khacnhat16@gmail.com>
2026-09-26 19:27:51 +0700

    fix: update backend

--- agrilog-backend/dist/server.js
+++ agrilog-backend/dist/server.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// import dns from 'dns';
+// dns.setServers(['8.8.8.8', '1.1.1.1']);
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
